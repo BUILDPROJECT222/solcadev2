744014e69b07912f1bbe9b7dd97890091386694d
kuyol-project <153427860+BUILDPROJECT222@users.noreply.github.com>
2024-03-11 21:09:24 +0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,32 +1,56 @@
 // config.ts
 
-import { GambaStandardTokens, TokenMeta } from "gamba-react-ui-v2";
+import {
+  FAKE_TOKEN_MINT,
+  GambaStandardTokens,
+  useTokenMeta,
+} from "gamba-react-ui-v2";
 
 import { PublicKey } from "@solana/web3.js";
+import { TokenMetadata } from "./types";
 
 // Solana address you wish to receive fees
 export const PLATFORM_CREATOR_ADDRESS = new PublicKey(
   "4H78a8exeM6HDetdgpCFeQTo2jZcypwiGoebW9qrUG1s",
 );
 
-// Platform URL - Appears in ShareModal
-export const PLATFORM_SHARABLE_URL = "play-Solcade";
+// Creator fee (in %)
+export const PLATFORM_CREATOR_FEE = 0.05; // 5% (5/100 = 0.05)
 
-// Toggle all live events acrossed gamba toast
+// Jackpot fee (in %)
+export const PLATFORM_JACKPOT_FEE = 0.01; // 1% (1/100 = 0.01)
+
+// Platform URL - Appears in ShareModal
+export const PLATFORM_SHARABLE_URL = "play.solcade.net";
+
+// Toggle all live events acrossed gamba toast (true = on, false = off)
 export const LIVE_EVENT_TOAST = true;
 
-// RPC - Can be configured in .env
-export const RPC_ENDPOINT =
-  process.env.NEXT_PUBLIC_RPC_ENDPOINT ?? "https://api.mainnet-beta.solana.com";
-
-// Platform explorer URL - Appears in welcome banner
+// Platform explorer URL - Appears in welcome banner (can be changed for if you have cusotm explorer)
 export const PLATFORM_EXPLORER_URL = `https://explorer.gamba.so/platform/${PLATFORM_CREATOR_ADDRESS.toString()}`;
 
 // List of tokens supported by this platform
-export const TOKENS: TokenMeta[] = [
-  GambaStandardTokens.sol,
-  GambaStandardTokens.usdc,
-
+export const TOKENS: Record<string, TokenMetadata> = {
+  // SOL
+  So11111111111111111111111111111111111111112: {
+    mint: new PublicKey("So11111111111111111111111111111111111111112"),
+    symbol: "SOL",
+    name: "Solana",
+    image:
+      "https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png",
+    decimals: 9,
+    baseWager: 0.01e9,
+  },
+  // USDC
+  EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v: {
+    mint: new PublicKey("EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v"),
+    symbol: "USDC",
+    name: "USD Coin",
+    image:
+      "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAMAAACdt4HsAAAAM1BMVEUAAAAgcM8oeMcndckndcondco1fs1ChtFQj9RdmNdroNt4qd6TuuW81O/X5vXl7vn///9JjkDLAAAABXRSTlMAECDg8En8Fj4AAAFcSURBVFjDvZfRQsMgDEXp2minzuX/v9YXnaXJvbktzjw1QA6BQkhagzLbQ5Z2VCyRQXMdMRuR5fzsohcmyKg9IZgso/Y5YT/mfausLyWh73d3Z3okBPMI2CEw4OYI4J8QEMe6JU51rdw+bjwl0MXCPsU+ahlhxvMnakIofh9ZRQqgB2o3ZKrsDfzP3oXCX7oq5oBvhbnAdxwQMkDmwO/nFbiwA4B5SNfmFPWjriLAHg689aM+8JUwd/dbANQxCKxBPLRnATlFAsCDJAN6xCmA2Vpfh1bv+n0UEO/5YcCNARYcPyUNXaZOI6G6MYB2G8V44HU82M2zyhGpsZ9VxUQxKhuNytiF+l24jL5M30/bggnpquLzfPZ1ThIEFshC74QzDMfmXiVJKBA6tS/TtJ+2VyVPNAw4lqrSBiVZJqqWrj+zYHhWyfMHRZeAmAYLR6l2nZD15d+K7wjBo74A10JdTYrgUpYAAAAASUVORK5CYII=",
+    decimals: 6,
+    baseWager: 1e6,
+  },
   {
     mint: new PublicKey("7vuhsRQ2gE4WPv37qegBKu8PcWHxDb5rQ6fQKkDfUghF"),
     symbol: 'SOLCADE',
@@ -35,7 +59,6 @@ export const TOKENS: TokenMeta[] = [
     decimals: 6,
     baseWager: 10 * 1e6,
   },
-
   {
     mint: new PublicKey("JUPyiwrYJFskUPiHa7hkeR8VUtAeFoSYbKedZNsDvCN"),
     symbol: 'JUP',
@@ -44,14 +67,28 @@ export const TOKENS: TokenMeta[] = [
     decimals: 6,
     baseWager: 1 * 1e6,
   },
+  // You can easily add more tokens here
+  //
+  // "000000000000000000000000000000000000000000": {
+  //   mint: new PublicKey("000000000000000000000000000000000"),
+  //   symbol: "",
+  //   name: "",
+  //   image: "",
+  //   decimals: 9,
+  //   baseWager: 1e9,
+  // },
+  //
+};
 
-  // Uncomment and configure this part if you have a custom SPL token you want to add
-  // {
-  //   mint: new PublicKey("Your_Token's_PublicKey"),
-  //   symbol: 'Your_Token_Symbol',
-  //   name: 'Your_Token_Name',
-  //   image: "Your_Token_Image_URL",
-  //   decimals: Your_Token_Decimals,
-  //   baseWager: Your_Base_Wager_Value,
-  // }
-];
+// Fallback handler to utilize the tokens dictionary for metadata lookup
+useTokenMeta.setFallbackHandler((mint) => {
+  const mintAddress = mint.toString();
+  const token = TOKENS[mintAddress];
+  if (token) {
+    return {
+      ...token,
+      mint: new PublicKey(token.mint),
+    };
+  }
+  return undefined;
+});
